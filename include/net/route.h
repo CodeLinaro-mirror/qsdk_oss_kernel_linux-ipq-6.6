665f257e04d6534e2001e3db73c0922a2968b48b
Murat Sezgin <msezgin@codeaurora.org>
2016-02-10 15:38:12 -0800

    net: ipv4/ipv6: Added route table update notify chain
    Registered modules are notified for the changes in the
    route table.
    
    NOTE: This patch is originally suggested by "Jozsef Kadlecsik"
    in the following email chain but never merged to any of the
    kernel versions.
    http://www.spinics.net/lists/netfilter-devel/msg24239.html
    
    Cc: Jozsef Kadlecsik <kadlec@blackhole.kfki.hu>
    Patch-mainline: netfilter-devel@vger.kernel.org
    
    Change-Id: I402710474be0978867dfbbbb8d1a7f8c8f3dab06
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- include/net/route.h
+++ include/net/route.h
@@ -237,6 +237,9 @@ struct rtable *rt_dst_alloc(struct net_device *dev,
 			    unsigned int flags, u16 type, bool noxfrm);
 struct rtable *rt_dst_clone(struct net_device *dev, struct rtable *rt);
 
+int ip_rt_register_notifier(struct notifier_block *nb);
+int ip_rt_unregister_notifier(struct notifier_block *nb);
+
 struct in_ifaddr;
 void fib_add_ifaddr(struct in_ifaddr *);
 void fib_del_ifaddr(struct in_ifaddr *, struct in_ifaddr *);
