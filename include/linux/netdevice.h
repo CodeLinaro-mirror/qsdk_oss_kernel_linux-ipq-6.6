98f71e72955490c9a1dcdb0f290e4a874a92ae4e
pavir <pavir@codeaurora.org>
2016-12-26 14:41:20 +0530

    net: l2tp: ppp_channel_proto_version API
    New API added to retrieve channel version info
    
    Change-Id: Ia4fd236c7cf6170a5034609e75de8c6e4ef79f7b
    Signed-off-by: Shyam Sunder <ssunde@codeaurora.org>
    Signed-off-by: pavir <pavir@codeaurora.org>
    
    ppp: Fix ppp_connect_channel function's return value
    
    The function should return 0 after all the successful
    checks. The patch fixes failed PPPoE connections.
    
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>
    Change-Id: I6c4755b0b158ca9bd75403e01cb3c0dda193e9e0
    Signed-off-by: Amruth S <quic_amrus@quicinc.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2196,7 +2196,6 @@ struct net_device {
 	unsigned char		nested_level;
 #endif
 
-
 	/* Protocol-specific pointers */
 
 	struct in_device __rcu	*ip_ptr;
--- include/linux/ppp_channel.h
+++ include/linux/ppp_channel.h
@@ -44,6 +44,8 @@ struct ppp_channel_ops {
 	 * the channel subtype
 	 */
 	int (*get_channel_protocol)(struct ppp_channel *);
+	/* Get channel protocol version */
+	int (*get_channel_protocol_ver)(struct ppp_channel *);
 	/* Hold the channel from being destroyed */
 	void (*hold)(struct ppp_channel *);
 	/* Release hold on the channel */
@@ -67,6 +69,9 @@ struct ppp_channel {
  */
 extern int ppp_channel_get_protocol(struct ppp_channel *);
 
+/* Call this get protocol version */
+extern int ppp_channel_get_proto_version(struct ppp_channel *);
+
 /* Call this to hold a channel */
 extern bool ppp_channel_hold(struct ppp_channel *);
 
@@ -83,6 +88,10 @@ extern int ppp_hold_channels(struct net_device *dev,
 			     unsigned int chan_sz);
 
 bool ppp_is_cp_enabled(struct net_device *dev);
+
+/* Test if ppp xmit lock is locked */
+extern bool ppp_is_xmit_locked(struct net_device *dev);
+
 /* Hold PPP channels for the PPP device */
 extern int __ppp_hold_channels(struct net_device *dev,
 			       struct ppp_channel *channels[],
