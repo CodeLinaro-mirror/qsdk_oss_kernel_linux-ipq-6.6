c9e8cfc5a7e5d2ded5e9934272af7b9b453183f1
Murat Sezgin <msezgin@codeaurora.org>
2017-05-08 11:49:21 -0700

    OpenWRT: 644-bridge_optimize_netfilter_hooks.patch
    This patch applies the following upstreamed
    openwrt patch from 3.14 kernel.
    
    commit 76b37c24252b (OpenWrt: apply 644-bridge_optimize_netfilter_hooks.patch)
    
    Change-Id: I4dde201c34717321115c9c067dc10d0b38d6eee6
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>
    (cherry picked from commit a647c9cf03fd82ce5e090891e5ffa68e22dcf88d)

--- net/bridge/br_input.c
+++ net/bridge/br_input.c
@@ -65,7 +65,7 @@ static int br_pass_frame_up(struct sk_buff *skb)
 	br_multicast_count(br, NULL, skb, br_multicast_igmp_type(skb),
 			   BR_MCAST_DIR_TX);
 
-	return NF_HOOK(NFPROTO_BRIDGE, NF_BR_LOCAL_IN,
+	return BR_HOOK(NFPROTO_BRIDGE, NF_BR_LOCAL_IN,
 		       dev_net(indev), NULL, skb, indev, NULL,
 		       br_netif_receive_skb);
 }
@@ -396,7 +396,7 @@ static rx_handler_result_t br_handle_frame(struct sk_buff **pskb)
 		 *   - returns = 0 (stolen/nf_queue)
 		 * Thus return 1 from the okfn() to signal the skb is ok to pass
 		 */
-		if (NF_HOOK(NFPROTO_BRIDGE, NF_BR_LOCAL_IN,
+		if (BR_HOOK(NFPROTO_BRIDGE, NF_BR_LOCAL_IN,
 			    dev_net(skb->dev), NULL, skb, skb->dev, NULL,
 			    br_handle_local_finish) == 1) {
 			return RX_HANDLER_PASS;
@@ -417,7 +417,7 @@ forward:
 		if (ether_addr_equal(p->br->dev->dev_addr, dest))
 			skb->pkt_type = PACKET_HOST;
 
-		if (NF_HOOK(NFPROTO_BRIDGE, NF_BR_PRE_ROUTING,
+		if (BR_HOOK(NFPROTO_BRIDGE, NF_BR_PRE_ROUTING,
 			dev_net(skb->dev), NULL, skb, skb->dev, NULL,
 			br_handle_local_finish) == 1) {
 			return RX_HANDLER_PASS;
