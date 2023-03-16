c593ca6c6ecc8e1067c6542082ebff9ebca1bb54
Felix Fietkau <nbd@nbd.name>
2023-03-16 19:26:06 +0530

    OpenWrt: 221-module_exports.patch
    add an optional config option for stripping all unnecessary symbol exports from the kernel image
    
    lede-commit: bb5a40c64b7c4f4848509fa0a6625055fc9e66cc
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    
    Change-Id: Ice6b20ec8b3dc4a2d1e850e0d179c882c58296a8
    Signed-off-by: Ram Chandra Jangir <quic_rjangir@quicinc.com>
    (cherry picked from commit 5c0ef6b9d947e66a74cec8cc57a878ea4c97925d)

--- include/linux/export-internal.h
+++ include/linux/export-internal.h
@@ -23,6 +23,12 @@
 #define __KSYM_REF(sym)		".long " #sym
 #endif
 
+#ifdef MODULE
+#define __EXPORT_SUFFIX(name)
+#else
+#define __EXPORT_SUFFIX(name) + #name
+#endif
+
 /*
  * For every exported symbol, do the following:
  *
@@ -35,7 +41,7 @@
  * former apparently works on all arches according to the binutils source.
  */
 #define __KSYMTAB(name, sym, sec, ns)						\
-	asm("	.section \"__ksymtab_strings\",\"aMS\",%progbits,1"	"\n"	\
+	asm("	.section \"__ksymtab_strings __EXPORT_SUFFIX(name)\",\"aMS\",%progbits,1"	"\n"	\
 	    "__kstrtab_" #name ":"					"\n"	\
 	    "	.asciz \"" #name "\""					"\n"	\
 	    "__kstrtabns_" #name ":"					"\n"	\
