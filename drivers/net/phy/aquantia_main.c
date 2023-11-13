f0dd774062b5afe5ee6f573c1d2840c6b020de9f
zhongjia <quic_zhongjia@quicinc.com>
2023-11-13 13:19:33 +0800

    drivers: aquantia: support MVLX3410 PHY
    Change-Id: I5beb2cbdbf3fe4ac7e42f9b261170db1117dda77
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>
    Signed-off-by: Lei Wei <quic_leiwei@quicinc.com>

--- drivers/net/phy/aquantia_main.c
+++ drivers/net/phy/aquantia_main.c
@@ -35,6 +35,7 @@
 #define PHY_ID_AQR109	0x03a1b502
 #define PHY_ID_AQR111	0x03a1b610
 #define PHY_ID_AQR111B0	0x03a1b612
+#define PHY_ID_MVLX3410	0x31c31dd3
 
 #define MDIO_PHYXS_VEND_IF_STATUS		0xe812
 #define MDIO_PHYXS_VEND_IF_STATUS_TYPE_MASK	GENMASK(7, 3)
@@ -1119,6 +1120,25 @@ static struct phy_driver aqr_driver[] = {
 	.get_stats	= aqr107_get_stats,
 	.link_change_notify = aqr107_link_change_notify,
 },
+{
+	PHY_ID_MATCH_MODEL(PHY_ID_MVLX3410),
+	.name           = "MVLX3410",
+	.probe          = aqr107_probe,
+	.get_rate_matching = aqr107_get_rate_matching,
+	.config_init    = aqr107_config_init,
+	.config_aneg    = aqr_config_aneg,
+	.config_intr    = aqr_config_intr,
+	.handle_interrupt       = aqr_handle_interrupt,
+	.read_status    = aqr113c_read_status,
+	.get_tunable    = aqr107_get_tunable,
+	.set_tunable    = aqr107_set_tunable,
+	.suspend        = aqr107_suspend,
+	.resume         = aqr107_resume,
+	.get_sset_count = aqr107_get_sset_count,
+	.get_strings    = aqr107_get_strings,
+	.get_stats      = aqr107_get_stats,
+	.link_change_notify = aqr107_link_change_notify,
+},
 };
 
 module_phy_driver(aqr_driver);
@@ -1142,6 +1162,7 @@ static struct mdio_device_id __maybe_unused aqr_tbl[] = {
 	{ PHY_ID_MATCH_MODEL(PHY_ID_AQR109) },
 	{ PHY_ID_MATCH_MODEL(PHY_ID_AQR111) },
 	{ PHY_ID_MATCH_MODEL(PHY_ID_AQR111B0) },
+	{ PHY_ID_MATCH_MODEL(PHY_ID_MVLX3410) },
 	{ }
 };
 
