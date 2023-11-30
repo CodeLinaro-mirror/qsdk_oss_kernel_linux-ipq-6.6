686467350b62783e824166ad335c5903a37cc59d
Sridharan S N <quic_sridsn@quicinc.com>
2023-11-30 10:54:54 +0530

    remoteproc: qcom: Add NOTIFY_FATAL event type to SSR subdevice
    Currently the SSR subdevice notifies the client driver on crash of the
    rproc from the recovery workqueue using the BEFORE_SHUTDOWN event.
    However the client driver might be interested to know that the device
    has crashed as soon as possible atomically.
    
    Add NOTIFY_FATAL event to SSR subdevice to atomically notify rproc has
    crashed to the client driver.
    
    Change-Id: Ia6b97007e34fc3898f14c90ca06aa44565484a4e
    Signed-off-by: Sridharan S N <quic_sridsn@quicinc.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1139,6 +1139,16 @@ static void rproc_unprepare_subdevices(struct rproc *rproc)
 	}
 }
 
+static void rproc_notify_crash_subdevices(struct rproc *rproc)
+{
+	struct rproc_subdev *subdev;
+
+	list_for_each_entry_reverse(subdev, &rproc->subdevs, node) {
+		if (subdev->notify_crash)
+			subdev->notify_crash(subdev);
+	}
+}
+
 /**
  * rproc_alloc_registered_carveouts() - allocate all carveouts registered
  * in the list
@@ -2688,6 +2698,8 @@ void rproc_report_crash(struct rproc *rproc, enum rproc_crash_type type)
 
 	dev_err(&rproc->dev, "crash detected in %s: type %s\n",
 		rproc->name, rproc_crash_to_string(type));
+
+	rproc_notify_crash_subdevices(rproc);
 
 	queue_work(rproc_recovery_wq, &rproc->crash_handler);
 }
