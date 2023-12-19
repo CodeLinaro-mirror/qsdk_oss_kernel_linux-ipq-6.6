CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-5.4-rt

bridge: Fix the bridge stats update function
1. For updating the stats, use the current core's stats pointer
2. Hold the exclusive access to the sequence counter while updating
   the bridge stats.

Change-Id: I16d33677ee402dc4436741b630dfb9eb17caced0
Signed-off-by: Manish Verma <quic_maniverm@quicinc.com>

net/bridge/br_if.c
@@ -859,14 +859,16 @@ void br_dev_update_stats(struct net_device *dev,
 		return;
 
 	br = netdev_priv(dev);
-	stats = per_cpu_ptr(br->stats, 0);
+	stats = this_cpu_ptr(br->stats);
 
+	local_bh_disable();
 	u64_stats_update_begin(&stats->syncp);
 	stats->rx_packets += nlstats->rx_packets;
 	stats->rx_bytes += nlstats->rx_bytes;
 	stats->tx_packets += nlstats->tx_packets;
 	stats->tx_bytes += nlstats->tx_bytes;
 	u64_stats_update_end(&stats->syncp);
+	local_bh_enable();
 }
 EXPORT_SYMBOL_GPL(br_dev_update_stats);
 

CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.6

net/bridge/br_if.c
@@ -859,14 +859,16 @@ void br_dev_update_stats(struct net_device *dev,
 	if (!(dev->priv_flags & IFF_EBRIDGE))
 		return;
 
-	stats = per_cpu_ptr(dev->tstats, 0);
+	stats = this_cpu_ptr(dev->tstats);
 
+	local_bh_disable();
 	u64_stats_update_begin(&stats->syncp);
 	u64_stats_add(&stats->rx_packets, nlstats->rx_packets);
 	u64_stats_add(&stats->rx_bytes, nlstats->rx_bytes);
 	u64_stats_add(&stats->tx_packets, nlstats->tx_packets);
 	u64_stats_add(&stats->tx_bytes, nlstats->tx_bytes);
 	u64_stats_update_end(&stats->syncp);
+	local_bh_enable();
 }
 EXPORT_SYMBOL_GPL(br_dev_update_stats);
 