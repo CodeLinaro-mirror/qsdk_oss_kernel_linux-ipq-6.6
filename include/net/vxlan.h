02d4a6aaa370c93878fc21ad812f219529759c3d
Sachin Kohli <quic_sachkohl@quicinc.com>
2023-12-19 10:15:33 +0530

    net: vxlan: Adding APIs to VxLAN driver.
    Adding new APIs to verify VxLAN netdevice and
    update mac entries in VxLAN's fdb.
    Change-Id: I22962d4845cba3a258c095f6424557a29d3b354b
    Signed-off-by: Apoorv Gupta <apoogupt@codeaurora.org>
    
    net: vxlan: Added vxlan fdb notify chain
    
    Registered modules are notified based on following events:
    1. RTM_GETNEIGH
    2. RTM_NEWNEIGH
    3. RTM_DELNEIGH
    
    Change-Id: I0802b305a829800cafbabd4728c3c47ff3679938
    Signed-off-by: Cemil Coskun <ccoskun@codeaurora.org>
    Signed-off-by: Apoorv Gupta <apoogupt@codeaurora.org>
    Signed-off-by: Vishnu Vardhan Bantanahal <quic_vishvard@quicinc.com>

--- include/net/vxlan.h
+++ include/net/vxlan.h
@@ -352,6 +352,19 @@ struct vxlan_dev {
 					 VXLAN_F_VNIFILTER         |    \
 					 VXLAN_F_LOCALBYPASS)
 
+/*
+ * Application data for fdb notifier event
+ */
+struct vxlan_fdb_event {
+	struct net_device *dev;
+	struct vxlan_rdst *rdst;
+	u8 eth_addr[ETH_ALEN];
+};
+
+extern void vxlan_fdb_register_notify(struct notifier_block *nb);
+extern void vxlan_fdb_unregister_notify(struct notifier_block *nb);
+extern void vxlan_fdb_update_mac(struct vxlan_dev *vxlan, const u8 *mac, uint32_t vni);
+
 struct net_device *vxlan_dev_create(struct net *net, const char *name,
 				    u8 name_assign_type, struct vxlan_config *conf);
 
@@ -438,6 +451,22 @@ static inline __be32 vxlan_compute_rco(unsigned int start, unsigned int offset)
 	if (offset == offsetof(struct udphdr, check))
 		vni_field |= VXLAN_RCO_UDP;
 	return vni_field;
+}
+
+/*
+ * is_vxlan_dev()
+ *       Check if it is a VxLAN netdevice.
+ */
+static inline bool is_vxlan_dev(const struct net_device *dev)
+{
+	if (!dev)
+		return false;
+
+	if ((dev->dev.type) &&
+		!strncmp(dev->dev.type->name, "vxlan", sizeof("vxlan"))) {
+			return true;
+	}
+	return false;
 }
 
 static inline unsigned short vxlan_get_sk_family(struct vxlan_sock *vs)
