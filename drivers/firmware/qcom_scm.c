CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.1

firmware: qcom_scm: enable secure crypto commands
secure crypto commands are used by dm-crypt driver for encryption
and decryption of data before being written into nand partition
and after being read from it.

enable these commands to enable secure nand feature.

Change-Id: Ia8934bd72e5b7268d84c8d06b151ef872e270157
Signed-off-by: Poovendhan Selvaraj <quic_poovendh@quicinc.com>

drivers/firmware/qcom_scm.c
@@ -398,6 +398,24 @@ int qcom_scm_set_cold_boot_addr(void *entry)
 }
 EXPORT_SYMBOL(qcom_scm_set_cold_boot_addr);
 
+/*
+ * qcom_qcekey_release_xpu_prot() - release XPU protection
+ */
+int qcom_qcekey_release_xpu_prot(void)
+{
+	int ret;
+	struct qcom_scm_res res;
+	struct qcom_scm_desc desc = {
+		.svc = QCOM_SCM_QCE_CRYPTO_SIP,
+		.cmd = QCOM_SCM_QCE_UNLOCK_CMD,
+		.arginfo = QCOM_SCM_ARGS(0, QCOM_SCM_VAL),
+	};
+
+	ret = qcom_scm_call(__scm->dev, &desc, &res);
+	return ret ? : res.result[0];
+}
+EXPORT_SYMBOL(qcom_qcekey_release_xpu_prot);
+
 /**
  * qcom_scm_cpu_power_down() - Power down the cpu
  * @flags:	Flags to flush cache

CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.6

drivers/firmware/qcom_scm.c
@@ -400,6 +400,24 @@ int qcom_scm_set_cold_boot_addr(void *entry)
 }
 EXPORT_SYMBOL_GPL(qcom_scm_set_cold_boot_addr);
 
+/*
+ * qcom_qcekey_release_xpu_prot() - release XPU protection
+ */
+int qcom_qcekey_release_xpu_prot(void)
+{
+	int ret;
+	struct qcom_scm_res res;
+	struct qcom_scm_desc desc = {
+		.svc = QCOM_SCM_QCE_CRYPTO_SIP,
+		.cmd = QCOM_SCM_QCE_UNLOCK_CMD,
+		.arginfo = QCOM_SCM_ARGS(0, QCOM_SCM_VAL),
+	};
+
+	ret = qcom_scm_call(__scm->dev, &desc, &res);
+	return ret ? : res.result[0];
+}
+EXPORT_SYMBOL_GPL(qcom_qcekey_release_xpu_prot);
+
 /**
  * qcom_scm_cpu_power_down() - Power down the cpu
  * @flags:	Flags to flush cache