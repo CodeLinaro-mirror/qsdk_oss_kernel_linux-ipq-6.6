CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.1

qcom : scm : Add scm call support for ECDSA blob fetch
Adding an scm call to get the ECDSA blob from TME-L
by passing NONCE buffer.

Change-Id: I5f05b9806df4f874a4c5adcdb0d7e13c1fa7fe1d
Signed-off-by: Hariharan K <quic_harihk@quicinc.com>

drivers/firmware/qcom_scm.c
@@ -1866,6 +1866,68 @@ int __qti_scm_tz_hvc_log(struct device *dev, u32 svc_id, u32 cmd_id,
 }
 
 /**
+ * __qti_scm_get_ecdsa_blob() - Get the ECDSA blob from TME-L by sending NONCE
+ *
+ * @svc_id: SCM service id
+ * @cmd_id: SCM command id
+ * nonce_buf: NONCE buffer which contains the NONCE recieved from Q6.
+ * nonce_buf_len: Variable for NONCE buffer length
+ * ecdsa_buf: ECDSA buffer, used to receive the ECDSA blob from TME
+ * ecdsa_buf_len: Variable which holds the total ECDSA buffer lenght
+ * *ecdsa_consumed_len: Pointer to get the consumed ECDSA buffer lenght from TME
+ *
+ * This function can be used to get the ECDSA blob from TME-L by passing the
+ * NONCE through nonce_buf. nonce_buf and ecdsa_buf should be DMA alloc
+ * coherent and caller should take care of it.
+ */
+int __qti_scm_get_ecdsa_blob(struct device *dev, u32 svc_id, u32 cmd_id,
+		dma_addr_t nonce_buf, u32 nonce_buf_len, dma_addr_t ecdsa_buf,
+		u32 ecdsa_buf_len, u32 *ecdsa_consumed_len)
+{
+	int ret;
+	struct qcom_scm_res res;
+	struct qcom_scm_desc desc = {
+		.svc = svc_id,
+		.cmd = cmd_id,
+		.owner = ARM_SMCCC_OWNER_SIP,
+		.arginfo = QCOM_SCM_ARGS(5, QCOM_SCM_VAL, QCOM_SCM_VAL,
+					QCOM_SCM_VAL, QCOM_SCM_VAL, QCOM_SCM_RW),
+	};
+
+	dma_addr_t dma_ecdsa_consumed_len;
+
+	dma_ecdsa_consumed_len = dma_map_single(dev, ecdsa_consumed_len,
+			sizeof(u32), DMA_FROM_DEVICE);
+	ret = dma_mapping_error(dev, dma_ecdsa_consumed_len);
+	if (ret != 0) {
+		pr_err("%s: DMA Mapping Error : %d\n", __func__, ret);
+		return ret;
+	}
+
+	desc.args[0] = nonce_buf;
+	desc.args[1] = nonce_buf_len;
+	desc.args[2] = ecdsa_buf;
+	desc.args[3] = ecdsa_buf_len;
+	desc.args[4] = dma_ecdsa_consumed_len;
+
+	ret = qcom_scm_call(dev, &desc, &res);
+	dma_unmap_single(dev, dma_ecdsa_consumed_len, sizeof(u32), DMA_FROM_DEVICE);
+
+	return ret ? : res.result[0];
+}
+
+int qti_scm_get_ecdsa_blob(u32 svc_id, u32 cmd_id, dma_addr_t nonce_buf,
+			u32 nonce_buf_len, dma_addr_t ecdsa_buf,
+			u32 ecdsa_buf_len, u32 *ecdsa_consumed_len)
+{
+	int ret;
+	ret = __qti_scm_get_ecdsa_blob(__scm->dev, svc_id, cmd_id, nonce_buf,
+			nonce_buf_len, ecdsa_buf, ecdsa_buf_len, ecdsa_consumed_len);
+	return ret;
+}
+EXPORT_SYMBOL(qti_scm_get_ecdsa_blob);
+
+/**
  * __qti_scm_get_smmustate () - Get SMMU state
  * @svc_id: SCM service id
  * @cmd_id: SCM command id

CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.6

drivers/firmware/qcom_scm.c
@@ -1987,6 +1987,68 @@ int __qti_scm_tz_hvc_log(struct device *dev, u32 svc_id, u32 cmd_id,
 }
 
 /**
+ * __qti_scm_get_ecdsa_blob() - Get the ECDSA blob from TME-L by sending NONCE
+ *
+ * @svc_id: SCM service id
+ * @cmd_id: SCM command id
+ * nonce_buf: NONCE buffer which contains the NONCE recieved from Q6.
+ * nonce_buf_len: Variable for NONCE buffer length
+ * ecdsa_buf: ECDSA buffer, used to receive the ECDSA blob from TME
+ * ecdsa_buf_len: Variable which holds the total ECDSA buffer lenght
+ * *ecdsa_consumed_len: Pointer to get the consumed ECDSA buffer lenght from TME
+ *
+ * This function can be used to get the ECDSA blob from TME-L by passing the
+ * NONCE through nonce_buf. nonce_buf and ecdsa_buf should be DMA alloc
+ * coherent and caller should take care of it.
+ */
+int __qti_scm_get_ecdsa_blob(struct device *dev, u32 svc_id, u32 cmd_id,
+		dma_addr_t nonce_buf, u32 nonce_buf_len, dma_addr_t ecdsa_buf,
+		u32 ecdsa_buf_len, u32 *ecdsa_consumed_len)
+{
+	int ret;
+	struct qcom_scm_res res;
+	struct qcom_scm_desc desc = {
+		.svc = svc_id,
+		.cmd = cmd_id,
+		.owner = ARM_SMCCC_OWNER_SIP,
+		.arginfo = QCOM_SCM_ARGS(5, QCOM_SCM_VAL, QCOM_SCM_VAL,
+					QCOM_SCM_VAL, QCOM_SCM_VAL, QCOM_SCM_RW),
+	};
+
+	dma_addr_t dma_ecdsa_consumed_len;
+
+	dma_ecdsa_consumed_len = dma_map_single(dev, ecdsa_consumed_len,
+			sizeof(u32), DMA_FROM_DEVICE);
+	ret = dma_mapping_error(dev, dma_ecdsa_consumed_len);
+	if (ret != 0) {
+		pr_err("%s: DMA Mapping Error : %d\n", __func__, ret);
+		return ret;
+	}
+
+	desc.args[0] = nonce_buf;
+	desc.args[1] = nonce_buf_len;
+	desc.args[2] = ecdsa_buf;
+	desc.args[3] = ecdsa_buf_len;
+	desc.args[4] = dma_ecdsa_consumed_len;
+
+	ret = qcom_scm_call(dev, &desc, &res);
+	dma_unmap_single(dev, dma_ecdsa_consumed_len, sizeof(u32), DMA_FROM_DEVICE);
+
+	return ret ? : res.result[0];
+}
+
+int qti_scm_get_ecdsa_blob(u32 svc_id, u32 cmd_id, dma_addr_t nonce_buf,
+			u32 nonce_buf_len, dma_addr_t ecdsa_buf,
+			u32 ecdsa_buf_len, u32 *ecdsa_consumed_len)
+{
+	int ret;
+	ret = __qti_scm_get_ecdsa_blob(__scm->dev, svc_id, cmd_id, nonce_buf,
+			nonce_buf_len, ecdsa_buf, ecdsa_buf_len, ecdsa_consumed_len);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(qti_scm_get_ecdsa_blob);
+
+/**
  * __qti_scm_get_smmustate () - Get SMMU state
  * @svc_id: SCM service id
  * @cmd_id: SCM command id