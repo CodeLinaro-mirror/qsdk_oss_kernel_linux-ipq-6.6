caf-backup/quic_la_kernel_msm

ipmr: change the data type of packet statistic to unsigned long long and fix printing error.
Change-Id: I79857a255d50d7bd5946b379a7c972610cc58a27
Signed-off-by: Tos Xu <txu@codeaurora.org>

net/ipv4/ipmr.c
@@ -2731,7 +2731,7 @@ static int ipmr_vif_seq_show(struct seq_file *seq, void *v)
 		const char *name =  vif->dev ? vif->dev->name : "none";
 
 		seq_printf(seq,
-			   "%2Zd %-10s %8ld %7ld  %8ld %7ld %05X %08X %08X\n",
+			   "%2Zd %-10s %8llu %7llu  %8llu %7llu %05X %08X %08X\n",
 			   vif - mrt->vif_table,
 			   name, vif->bytes_in, vif->pkt_in,
 			   vif->bytes_out, vif->pkt_out,

CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.6

net/ipv4/ipmr.c
@@ -3180,7 +3180,7 @@ static int ipmr_vif_seq_show(struct seq_file *seq, void *v)
 		vif_dev = vif_dev_read(vif);
 		name = vif_dev ? vif_dev->name : "none";
 		seq_printf(seq,
-			   "%2td %-10s %8ld %7ld  %8ld %7ld %05X %08X %08X\n",
+			   "%2td %-10s %8llu %7llu  %8llu %7llu %05X %08X %08X\n",
 			   vif - mrt->vif_table,
 			   name, vif->bytes_in, vif->pkt_in,
 			   vif->bytes_out, vif->pkt_out,