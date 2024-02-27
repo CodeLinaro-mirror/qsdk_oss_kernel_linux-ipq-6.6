d3b0cf855ff0b9d0405fa54b11496f228acfd7d3
Saahil Tomar <quic_saahtoma@quicinc.com>
2024-02-27 16:53:57 +0530

    crypto: qce - Resolve [-Werror=enum-conversion] warning.
    This change resolves the warnings caused by dma_transfer_direction
    and dma_data_direction enums being used interchangeably leading to
    warnings due to incompatible type conversions.
    
    Change-Id: I081a2f357e786e27bbfca2ea3c69635c5a3021f1
    Signed-off-by: Saahil Tomar <quic_saahtoma@quicinc.com>

--- drivers/crypto/qce/skcipher.c
+++ drivers/crypto/qce/skcipher.c
@@ -62,12 +62,12 @@ static void qce_skcipher_done(void *data)
 			dma_unmap_sg(qce->dev,
 				qce_bam_txn->qce_reg_read_sgl,
 				qce_bam_txn->qce_read_sgl_cnt,
-				DMA_DEV_TO_MEM);
+				dir_dst);
 		if (qce_bam_txn->qce_write_sgl_cnt)
 			dma_unmap_sg(qce->dev,
 				qce_bam_txn->qce_reg_write_sgl,
 				qce_bam_txn->qce_write_sgl_cnt,
-				DMA_MEM_TO_DEV);
+				dir_src);
 	}
 
 	memcpy(rctx->iv, result_buf->encr_cntr_iv, rctx->ivsize);
