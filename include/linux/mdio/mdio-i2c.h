52dd5c4f07ae333fd354d0b97f769e0afe962d47
zhongjia <quic_zhongjia@quicinc.com>
2024-05-08 01:22:06 +0800

    drivers: net: mdio: mdio-i2c support laguna
    Change-Id: I176be15061440d2558e9722fc4c7fb4a6a118d30
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- include/linux/mdio/mdio-i2c.h
+++ include/linux/mdio/mdio-i2c.h
@@ -16,6 +16,20 @@ enum mdio_i2c_proto {
 	MDIO_I2C_MARVELL_C22,
 	MDIO_I2C_C45,
 	MDIO_I2C_ROLLBALL,
+	MIDO_I2C_QCOM,
+};
+
+struct qcom_mdio_i2c_data {
+	void __iomem	*membase[2];
+	void __iomem *eth_ldo_rdy[3];
+	int clk_div;
+	bool force_c22;
+	struct gpio_descs *reset_gpios;
+	void (*preinit)(struct mii_bus *bus);
+	u32 (*sw_read)(struct mii_bus *bus, u32 reg);
+	void (*sw_write)(struct mii_bus *bus, u32 reg, u32 val);
+	void *clk[5];
+	struct i2c_adapter *i2c;
 };
 
 struct mii_bus *mdio_i2c_alloc(struct device *parent, struct i2c_adapter *i2c,
