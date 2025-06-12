a445aa5478dfc2c444dca526eae90888f58570b1
Timple Raj M <timple@qti.qualcomm.com>
2025-06-12 12:02:51 +0530

    drivers: mtd: nand: Add the NAND support
    This patch adds the NAND support for the IPQ52xx chipset
    
    Change-Id: I8c90762fcdabab10f32e61d9e6f8927abe0e1a05
    Signed-off-by: Timple Raj M <timple@qti.qualcomm.com>

--- drivers/mtd/nand/raw/qcom_nandc.c
+++ drivers/mtd/nand/raw/qcom_nandc.c
@@ -4374,6 +4374,17 @@ static const struct qcom_nandc_props ipq5424_nandc_props = {
 	.dev_cmd_reg_start = 0x7000,
 };
 
+static const struct qcom_nandc_props ipq5200_nandc_props = {
+	.ecc_modes = (ECC_BCH_4BIT | ECC_BCH_8BIT),
+	.is_bam = true,
+	.is_serial_nand = true,
+	.qpic_v2 = true,
+	.is_serial_training = true,
+	.quad_mode = true,
+	.page_scope = true,
+	.dev_cmd_reg_start = 0x7000,
+};
+
 static const struct qcom_nandc_props ipq9574_nandc_props = {
 	.ecc_modes = (ECC_BCH_4BIT | ECC_BCH_8BIT),
 	.is_bam = true,
@@ -4414,6 +4425,10 @@ static const struct of_device_id qcom_nandc_of_match[] = {
 	{
 		.compatible = "qcom,ipq5424-nand",
 		.data = &ipq5424_nandc_props,
+	},
+	{
+		.compatible = "qcom,ipq5200-nand",
+		.data = &ipq5200_nandc_props,
 	},
 	{
 		.compatible = "qcom,ipq9574-nand",
