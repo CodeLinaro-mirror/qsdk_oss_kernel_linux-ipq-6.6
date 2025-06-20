b80a3582afe9682103be4e480ddb4f765b352d75
Timple Raj M <timple@qti.qualcomm.com>
2025-06-20 11:22:03 +0530

    pcie: qcom: Add PCIe support for IPQ52xx chipset
    This patch adds the PCIe support for the IPQ52xx
    chipset
    
    Change-Id: If8619382e867cb7519dffa4541b78fea856116e0
    Signed-off-by: Timple Raj M <timple@qti.qualcomm.com>

--- drivers/pci/controller/dwc/pcie-qcom.c
+++ drivers/pci/controller/dwc/pcie-qcom.c
@@ -2308,6 +2308,7 @@ static const struct of_device_id qcom_pcie_match[] = {
 	{ .compatible = "qcom,pcie-ipq4019", .data = &cfg_2_4_0 },
 	{ .compatible = "qti,pcie-ipq5332", .data = &cfg_1_27_0 },
 	{ .compatible = "qcom,pcie-ipq5424", .data = &cfg_1_27_0 },
+	{ .compatible = "qcom,pcie-ipq5200", .data = &cfg_1_27_0 },
 	{ .compatible = "qcom,pcie-ipq6018", .data = &cfg_2_9_0 },
 	{ .compatible = "qcom,pcie-ipq8064", .data = &cfg_2_1_0 },
 	{ .compatible = "qcom,pcie-ipq8064-v2", .data = &cfg_2_1_0 },
