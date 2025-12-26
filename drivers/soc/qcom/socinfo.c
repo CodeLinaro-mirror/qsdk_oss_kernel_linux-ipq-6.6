95e3fcfd2d425f7f97c506ed4e041500336bdef6
Timple Raj M <timple@qti.qualcomm.com>
2025-12-26 16:03:30 +0530

    soc: qcom: socinfo: Add SoC ID of IPQ52xx and IPQ96xx
    This patch adds the SoC ID's of IPQ52xx and IPQ96xx
    chipsets in the socinfo driver.
    
    Change-Id: I6d64c596707a05f49f50af594e39a3cd1f8e4a24
    Signed-off-by: Timple Raj M <timple@qti.qualcomm.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -426,6 +426,13 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(IPQ5321) },
 	{ qcom_board_id(IPQ5424) },
 	{ qcom_board_id(IPQ5404) },
+	{ qcom_board_id(IPQ5200) },
+	{ qcom_board_id(IPQ5210) },
+	{ qcom_board_id(QCF2200) },
+	{ qcom_board_id(QCF3200) },
+	{ qcom_board_id(QCF3210) },
+	{ qcom_board_id(IPQ9620) },
+	{ qcom_board_id(IPQ9650) },
 };
 
 static const char *socinfo_machine(struct device *dev, unsigned int id)
