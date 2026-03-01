57bd541fe4408db8d1aecf0dca266ca16aab2239
Manikanta Mylavarapu <mmanikan@qti.qualcomm.com>
2026-03-01 23:59:00 +0530

    cpufreq: dt-platdev: add ipq5210 support
    Change-Id: I928b319210c182c76b8a343686a4ac101362d2e8
    Signed-off-by: Manikanta Mylavarapu <mmanikan@qti.qualcomm.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -178,6 +178,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "ti,am625", },
 	{ .compatible = "ti,am62a7", },
 
+	{ .compatible = "qcom,ipq5210", },
 	{ .compatible = "qcom,ipq5332", },
 	{ .compatible = "qcom,ipq5424", },
 	{ .compatible = "qcom,ipq8064", },
